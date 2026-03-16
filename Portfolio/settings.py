51d400a657134656509faf609a81e6355aefd05f
Preetam940 <preetampatel940@gmail.com>
2026-03-16 10:54:30 +0530

    deployment setup

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$lbm@*#2r)qm%gzdee*+ydi)641963v82-+sy!ytf!)-!pig1k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
